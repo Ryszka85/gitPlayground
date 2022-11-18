69253270f0983f80d309a59911498a97341d7357
Adrian <adrian.ryszka@gmx.net>
2022-11-18 19:55:22 +0100

    first commit

---
+++ src/test/java/io/ryszka/SomeTest.java
@@ -0,0 +1,14 @@
+package io.ryszka;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class SomeTest {
+
+    @Test
+    public void fooTest() {
+        assertThat("Foo").isEqualTo("Foo");
+    }
+
+}
